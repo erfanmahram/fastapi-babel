bf3e19f6ffa2b296a53e495c09aa8a5c60c69ec0
erfanmahram <erfanmahram@gmail.com>
2022-10-23 20:00:25 +0330

    lang_code handler fixed

--- fastapi_babel/middleware.py
+++ fastapi_babel/middleware.py
@@ -4,6 +4,7 @@ from starlette.middleware.base import RequestResponseEndpoint
 from starlette.middleware.base import DispatchFunction
 from starlette.types import ASGIApp
 from typing import TYPE_CHECKING
+from babel.core import Locale
 
 if TYPE_CHECKING:
     from .core import Babel
@@ -30,6 +31,13 @@ class InternationalizationMiddleware(BaseHTTPMiddleware):
         """
         lang_code: str = request.headers.get("Accept-Language", None)
         if lang_code:
+            if lang_code not in self.babel.config.ACCEPTABLE_LANGUAGES:
+                try:
+                    lang_code = Locale.parse(lang_code, sep='-').language
+                except:
+                    lang_code = self.babel.config.BABEL_DEFAULT_LOCALE
+                if lang_code not in self.babel.config.ACCEPTABLE_LANGUAGES:
+                    lang_code = self.babel.config.BABEL_DEFAULT_LOCALE
             self.babel.locale = lang_code
         response = await call_next(request)
         return response
--- fastapi_babel/properties.py
+++ fastapi_babel/properties.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 from dataclasses import dataclass, field
+from typing import List
 
 
 @dataclass
@@ -9,9 +10,10 @@ class RootConfigs:
     ROOT_DIR: str
     BABEL_DEFAULT_LOCALE: str
     BABEL_TRANSLATION_DIRECTORY: str
+    BABEL_MESSAGE_POT_FILE: str = field(init=False)
+    ACCEPTABLE_LANGUAGES: List[str]
     BABEL_DOMAIN: str = "messages.pot"
     BABEL_CONFIG_FILE: str = "babel.cfg"
-    BABEL_MESSAGE_POT_FILE: str = field(init=False)
 
     def __post_init__(self):
         self.ROOT_DIR = pathlib.Path(self.ROOT_DIR).parent
